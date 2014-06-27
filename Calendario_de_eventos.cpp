6ebcb1e92bc0ec8f44999df64e979d5cf171b49d
sandrorml27 <sandrorml27@hotmail.es>
2014-06-27 16:45:08 -0500

    resumen 27/06/2014
    AVANZADO HASTA INGRESO DE EVENTOS

--- Calendario_de_eventos.cpp
+++ Calendario_de_eventos.cpp
@@ -8,37 +8,108 @@
 #include <conio>    //utilizacion de la funcion cout
 #include <stdio>
 
-
-
+//--------------------------------------------------------------------------------//
+//--------------------------------------------------------------------------------//
+//--------------------------------------------------------------------------------//
 int Principal()
 {
 	int a;
 	cout<<"1.-Ingresar nuevo evento "<<endl;
    cout<<"2.-Ver mis eventos para hoy "<<endl;
    cout<<"3.-Ver mis eventos para toda la semana "<<endl;
+   cout<<"4.-SALIR "<<endl;
    cin>>a;
    return (a);
 }
-
+//--------------------------------------------------------------------------------//
+//--------------------------------------------------------------------------------//
+//--------------------------------------------------------------------------------//
 Ingresar_nuevo_evento()
 {
-	cout<<"1.-Nombre corto del evento"<<endl;
-   cout<<"2.-Descripción del evento"<<endl;
-   cout<<"3.-Fecha/Hora de Inicio"<<endl;
-   cout<<"4.-Fecha/Hora de Fin"<<endl;
+	ofstream archivo("C:/Users/Sistemas/Desktop/sandro/miarchivo.txt", ios::app);
+	clrscr ();
+	char nuevo_evento[100];
+   char descripcion_del_evento[100];
+   int dia;
+   int mes;
+   int anio;
 
+      	cout<<"ingrese el nombre de el nuevo evento: "<<endl;
+         cin>>nuevo_evento;
+      	cout<<"ingrese una descripcion del evento: "<<endl;
+         cin>>descripcion_del_evento;
+      	cout<<"ingrese el dia del evento: "<<endl;
+         cin>>dia;
+      	cout<<"ingrese el mes del evento: "<<endl;
+         cin>>mes;
+      	cout<<"ingrese el año del evento: "<<endl;
+         cin>>anio;
+         archivo<< nuevo_evento;
+         archivo<< descripcion_del_evento;
+         archivo<< dia;
+         archivo<< mes;
+         archivo<< anio;
+         clrscr ();
+         //Principal();
 }
+//--------------------------------------------------------------------------------//
+//--------------------------------------------------------------------------------//
+//--------------------------------------------------------------------------------//
+Ver_mis_eventos_para_hoy()
+{
+	cout<<"Fecha actual: "<<endl;
+   cout<<"lista de eventos "<<endl;
+}
+Ver_mis_eventos_para_toda_la_semana()
+{
+	cout<<"Semana actual: "<<endl;
+   cout<<"lista de eventos "<<endl;
+   getch();
+   Principal();
+}
+//--------------------------------------------------------------------------------//
+//--------------------------------------------------------------------------------//
+//--------------------------------------------------------------------------------//
 main()
 {
-   if (Principal()==1)
+	ifstream archivo("C:/Users/Sistemas/Desktop/sandro/miarchivo.txt", ios::in);
+   if (archivo)
    {
-   	Ingresar_nuevo_evento();
+   	ofstream archivo("C:/Users/Sistemas/Desktop/sandro/miarchivo.txt", ios::app);
    }
-   if (Principal()==2)
+   else
    {
-   	Ver_mis_eventos_para_hoy();
+   	ofstream archivo("C:/Users/Sistemas/Desktop/sandro/miarchivo.txt");
+   	archivo.close();
 
    }
+
+	int s;
+   do {
+   s=Principal();
+   switch(s)
+	{
+		case 1:
+			Ingresar_nuevo_evento();
+			break;
+
+		case 2:
+			Ver_mis_eventos_para_hoy();
+			break;
+
+  	 	case 3:
+			Ver_mis_eventos_para_toda_la_semana();
+  	   	 break;
+
+      case 4:
+			cout<<"precione una tecla para salir"<<endl;;
+  	   	 break;
+
+		default:
+			cout<<"Ha ingresado un numero no valido"<<endl;
+			break;
+	}
+   }while(s!=4);
 
    getch();
 }
